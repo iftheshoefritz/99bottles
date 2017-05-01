8a72ef3b10ebc696d370d71cfffe9845a1895f93
Fritz Meissner <fritz.meissner@gmail.com>
2017-05-01 18:00:27 +0200

    Listing 1: Incomprehensibly Concise

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,29 +8,12 @@ class Bottles
     starting.downto(ending).map {|i| verse(i)}.join("\n")
   end
 
-  def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
-    end
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+      " of beer on the wall, " +
+      "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+      "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around" : "Go to the store and buy some more"}, " +
+      "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+      " of beer on the wall.\n"
   end
-
 end
