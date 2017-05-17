240d03d4de5cd82f71e229a7af1217a46b57b915
Fritz Meissner <fritz.meissner@gmail.com>
2017-05-17 09:36:36 +0200

    refactoring as of end of talk 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,25 +11,40 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
+      "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{amount(number)} #{container(number)} of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
+      "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{amount(number)} #{container(number)} of beer.\n" +
+      "Take #{pronoun(number)} down and pass it around, " +
+      "#{amount(number-1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
+
+  def amount(n)
+    if n == 0
+      "no more"
+    else
+      n.to_s
+    end
+  end
+
+  def container(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(n)
+    if n == 1 
+      "it"
+    else
+      "one"
+    end
+  end
+
 end
