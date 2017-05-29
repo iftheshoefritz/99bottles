a6fe8fa01938188fe8d09f39e1b3b5b7f75c0b16
Fritz Meissner <fritz.meissner@gmail.com>
2017-05-29 17:53:48 +0200

    messages can be sent straight to bottle_number temp variable; straight-forward for the first few lines

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,9 +9,10 @@ class Bottles
   end
 
   def verse(number)
-    "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{amount(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
+    bottle_number = BottleNumber.new(number)
+    "#{bottle_number.amount.capitalize} #{bottle_number.container} of beer on the wall, " +
+    "#{bottle_number.amount} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number.action}, " +
     "#{amount(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
